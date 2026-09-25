540ba821f08738f3cd6943ce4a74b79c401a7475
akash <akashpaiasp@gmail.com>
2026-09-25 00:14:38 -0400

    added files

--- Downloads/BIOBUZZ3D-upload/1-github-repo/biobuzz3d/tools/fake_eos/heos/hcredentials.gd
+++
@@ -1,9 +0,0 @@
-extends RefCounted
-var product_name: String
-var product_version: String
-var product_id: String
-var sandbox_id: String
-var deployment_id: String
-var client_id: String
-var client_secret: String
-var encryption_key: String
